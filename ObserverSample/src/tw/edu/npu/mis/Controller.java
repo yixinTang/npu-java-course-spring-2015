c84db574374b8f6f40fc8372803394cfacaec2e5
Samael Wang <freesamael@gmail.com>
2015-05-17 11:59:43 +0800

    Add ObserverSample.

---
+++ ObserverSample/src/tw/edu/npu/mis/Controller.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2015, Samael Wang <freesamael@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package tw.edu.npu.mis;
+
+import java.util.Scanner;
+
+/**
+ * {@link Controller} retrieves inputs from the user.
+ *
+ * @author Samael Wang <freesamael@gmail.com>
+ */
+public class Controller {
+
+    private final Model mModel;
+
+    public Controller(Model model) {
+        mModel = model;
+    }
+
+    /**
+     * Read input and update model accordingly.
+     */
+    public void readInput() {
+        System.out.print("Controller: ");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        if (!input.trim().isEmpty()) {
+            mModel.setData(input);
+        }
+    }
+
+}

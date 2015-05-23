a3684c876ed2e20003181786fa10888cf07fa89b
Samael Wang <freesamael@gmail.com>
2015-05-24 01:17:13 +0800

    Add Calclator skeleton.

---
+++ Calculator/src/tw/edu/npu/mis/Calculator.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.edu.npu.mis;
+
+/**
+ * The model class of the calculator application.
+ */
+public class Calculator {
+    
+    /**
+     * The available operators of the calculator.
+     */
+    public enum Operator {
+        EQUAL,       // =
+        PLUS,        // +
+        MINUS,       // -
+        TIMES,       // ×
+        OVER,        // ⁄
+        PLUS_MINUS,  // ±
+        RECIPROCAL,  // 1/x
+        PERCENT,     // %
+        SQRT,        // √
+        BACKSPACE,   // ⌫
+        CLEAR,       // C
+        CLEAR_ENTRY, // CE
+        MEM_SET,     // MS
+        MEM_PLUS,    // M+
+        MEM_MINUS,   // M-
+        MEM_RECALL,  // MR
+        MEM_CLEAR    // MC
+    }
+    
+    public void appendDigit(int digit) {
+        // TODO code application logic here
+    }
+    
+    public void appendDot() {
+        // TODO code application logic here
+    }
+    
+    public void performOperation(Operator operator) {
+        // TODO code application logic here
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}

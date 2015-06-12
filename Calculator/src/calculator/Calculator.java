b94e7869f39bfe5dfec82bdfef585555e850706b
YiXIN <afg8352g@gmail.com>
2015-06-12 20:34:15 +0800

    1101410056

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+/**
+ * The model class of the calculator application.
+ */
+public class Calculator {
+    
+    /**
+     * The available operators of the calculator.
+     */
+    public enum Operator {
+        CLEAR,       // C
+        CLEAR_ENTRY, // CE
+        BACKSPACE,   // ⌫
+        EQUAL,       // =
+        PLUS,        // +
+        MINUS,       // -
+        TIMES,       // ×
+        OVER,        // ⁄
+        PLUS_MINUS,  // ±
+        RECIPROCAL,  // 1/x
+        PERCENT,     // %
+        SQRT,        // √
+        MEM_CLEAR,   // MC
+        MEM_SET,     // MS
+        MEM_PLUS,    // M+
+        MEM_MINUS,   // M-
+        MEM_RECALL   // MR
+    }
+    
+    public void appendDigit(int digit) {
+        // TODO code application logic here
+    }
+    
+    public void appendDot() {
+        // TODO code application logic here
+    }
+    
+    public void performOperation(Operator operator) {
+        // TODO code application logic here
+    }
+    
+    public String getDisplay() {
+        // TODO code application logic here
+        return null;
+    }}
+
+    /**
+     * @param args the command line arguments
+     */
+   
